51039f4ebe42ddb61337b33bcf8b831166575748
dimadimaya <103739636+dimadimaya@users.noreply.github.com>
2023-01-15 14:07:54 +0000

    Deploying to gh-pages from @ dimadimaya/goit-react-hw-05-movies@40cbc12fc8ed317123a25acea15855adfbd7df99 🚀

--- static/js/805.d63c9290.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,r){r.r(t);var n=r(87),o=r(184);t.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h1",{children:"Page not found"}),(0,o.jsx)(n.rU,{to:"/",children:"To Home"})]})}}}]);
-//# sourceMappingURL=805.d63c9290.chunk.js.map
